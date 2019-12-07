044d0d468d083537c049db0828d73ac4069dfcca
Erin Replogle <erinrep@gmail.com>
2019-12-07 14:43:13 -0800

    AoC 2019 Day 4 - part 2

--- 2019/4/potentialPasswords.exs
+++ 2019/4/potentialPasswords.exs
@@ -1,20 +1,28 @@
 defmodule Helpers do
-  def has_double([a, a, a, a, a, a]), do: true
-  def has_double([a, a, a, a, a, _e]), do: true
-  def has_double([a, a, a, a, _d, _e]), do: true
-  def has_double([a, a, a, _c, _d, _e]), do: true
-  def has_double([a, a, _b, _c, _d, _e]), do: true
-  def has_double([_a, b, b, b, b, b]), do: true
-  def has_double([_a, b, b, b, b, _e]), do: true
-  def has_double([_a, b, b, b, _d, _e]), do: true
-  def has_double([_a, b, b, _c, _d, _e]), do: true
-  def has_double([_a, _b, c, c, c, c]), do: true
-  def has_double([_a, _b, c, c, c, _e]), do: true
-  def has_double([_a, _b, c, c, _d, _e]), do: true
-  def has_double([_a, _b, _c, d, d, d]), do: true
-  def has_double([_a, _b, _c, d, d, _e]), do: true
-  def has_double([_a, _b, _c, _d, e, e]), do: true
-  def has_double(_any), do: false
+  def has_allowed_double([a, a, a, a, a, a]), do: false
+  def has_allowed_double([_a, b, b, b, b, _f]), do: false
+  def has_allowed_double([a, a, a, a, a, _f]), do: false
+  def has_allowed_double([_a, b, b, b, b, b]), do: false
+  def has_allowed_double([a, a, a, b, b, b]), do: false
+
+  def has_allowed_double([a, a, a, b, b, _f]), do: true
+  def has_allowed_double([a, a, a, _b, c, c]), do: true
+  def has_allowed_double([a, a, b, b, b, _f]), do: true
+  def has_allowed_double([a, a, _b, c, c, c]), do: true
+  def has_allowed_double([_a, b, b, b, c, c]), do: true
+  def has_allowed_double([_a, b, b, c, c, c]), do: true
+
+  def has_allowed_double([a, a, a, _d, _e, _f]), do: false
+  def has_allowed_double([_a, b, b, b, _e, _f]), do: false
+  def has_allowed_double([_a, _b, c, c, c, _f]), do: false
+  def has_allowed_double([_a, _b, _c, d, d, d]), do: false
+  
+  def has_allowed_double([a, a, _b, _c, _d, _f]), do: true
+  def has_allowed_double([_a, b, b, _c, _d, _f]), do: true
+  def has_allowed_double([_a, _b, c, c, _d, _f]), do: true
+  def has_allowed_double([_a, _b, _c, d, d, _f]), do: true
+  def has_allowed_double([_a, _b, _c, _d, e, e]), do: true
+  def has_allowed_double(_any), do: false
 
   def never_decreases(list), do: Enum.sort(list) == list
 end
@@ -25,17 +33,11 @@ from = 245318
 to = 765747
 
 to..from
-  |> Enum.map(fn candidate -> 
+  |> Enum.filter(fn candidate -> 
     parts = candidate
       |> Integer.to_string
       |> String.graphemes
-    {
-      candidate, 
-      Helpers.has_double(parts) && Helpers.never_decreases(parts)
-    }
-  end)
-  |> Enum.filter(fn {_candidate, fits_criteria} -> 
-    fits_criteria == true
+    Helpers.never_decreases(parts) && Helpers.has_allowed_double(parts)
   end)
   |> Enum.count
   |> IO.inspect

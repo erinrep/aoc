be6f04a5f9767a4031bc48288d3a5797a0b1829a
Erin Replogle <erinrep@gmail.com>
2019-12-07 23:12:30 -0800

    AoC Day 6 - part 2

--- 2019/6/orbitMap.exs
+++ 2019/6/orbitMap.exs
@@ -1,13 +1,13 @@
-IO.puts "AoC 2019 - Day 6: Universal Orbit Map"
-
 defmodule Helpers do
-  def total_orbits(orbits) do
-    planets = orbits
+  def get_planet_list(orbits) do
+    orbits
       |> Enum.flat_map(&(&1))
       |> MapSet.new
       |> MapSet.delete("COM")
-
-    direct_orbits = orbits
+  end
+  
+  def get_direct_orbits(orbits) do
+    orbits
       |> Enum.group_by(fn [a, _b] -> a end)
       |> Map.to_list
       |> Enum.map(fn {key, value} -> 
@@ -17,20 +17,56 @@ defmodule Helpers do
           |> MapSet.delete(key)
         {key, final}
       end)
-    
+  end
+
+  def get_total_orbits(planets, direct_orbits) do
     planets
-      |> Enum.map(&(count_hops(&1, direct_orbits, 0)))
+      |> Enum.map(&(count_hops("COM", &1, direct_orbits, 0)))
       |> Enum.sum
   end
 
-  def count_hops("COM", _orbits, hops), do: hops
-  def count_hops(planet, orbits, hops) do
+  def count_hops(root, root, _orbits, hops), do: hops
+  def count_hops(root, planet, orbits, hops) do
     {p, _children} = Enum.find(orbits, fn {_planet, children} -> 
       MapSet.member?(children, planet)
     end)
-    count_hops(p, orbits, hops + 1)
+    count_hops(root, p, orbits, hops + 1)
+  end
+
+  def get_common_ancestor(orbits, planet1, planet2) do
+    direct_orbits = get_direct_orbits(orbits)
+    
+    [a, b] = [planet1, planet2]
+      |> Enum.map(fn planet ->
+        planet
+          |> get_ancestors(direct_orbits, [])
+          |> MapSet.new
+      end)
+    
+    {common_ancestor, _hops} = a
+    |> MapSet.intersection(b)
+    |> MapSet.to_list
+    |> Enum.map(fn planet -> 
+      {planet, count_hops("COM", planet, direct_orbits, 0)}
+    end)
+    |> Enum.sort(fn ({_p1, h1}, {_p2, h2}) -> 
+      h1 < h2
+    end)
+    |> List.last
+
+    common_ancestor
+  end
+
+  def get_ancestors("COM", _orbits, ancestors), do: ancestors
+  def get_ancestors(planet, orbits, ancestors) do
+    {p, _children} = Enum.find(orbits, fn {_planet, children} -> 
+      MapSet.member?(children, planet)
+    end)
+    get_ancestors(p, orbits, ancestors ++ [p])
   end
 end
+
+IO.puts "AoC 2019 - Day 6: Universal Orbit Map"
 
 case File.read("localOrbits.txt") do
   {:ok, contents} ->
@@ -38,9 +74,15 @@ case File.read("localOrbits.txt") do
     |> String.split("\n", trim: true)
     |> Enum.map(&(String.split(&1, ")", trim: true)))
 
-    orbits
-      |> Helpers.total_orbits
-      |> IO.inspect()
+    planets = Helpers.get_planet_list(orbits)
+    direct_orbits = Helpers.get_direct_orbits(orbits)
+    
+    IO.inspect(Helpers.get_total_orbits(planets, direct_orbits))
+
+    common_ancestor = Helpers.get_common_ancestor(orbits, "YOU", "SAN")
+    hops_for_you = Helpers.count_hops(common_ancestor, "YOU", direct_orbits, 0)
+    hops_for_santa = Helpers.count_hops(common_ancestor, "SAN", direct_orbits, 0)
+    IO.inspect(hops_for_you + hops_for_santa - 2)
     
   {:error, :enoent} -> nil
 end

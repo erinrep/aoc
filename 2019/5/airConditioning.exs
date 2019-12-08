1e2bc18301492e401f430fa01b77d36e8ac5982b
Erin Replogle <erinrep@gmail.com>
2019-12-07 17:14:36 -0800

    streamline the pipeline

--- 2019/5/airConditioning.exs
+++ 2019/5/airConditioning.exs
@@ -1,57 +1,72 @@
 defmodule IntcodeComputer do
   def noun_spot, do: 1
   def verb_spot, do: 2
+  def system_id, do: 1 # air onditioner unit
 
-  def process_instruction(index, intcode) do
+  def process_instruction(intcode, index) do
     intcode
       |> Enum.drop(index)
       |> process_instruction(index, intcode)
   end
   def process_instruction([99 | _tail], _index, intcode), do: intcode
   def process_instruction([1, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, Enum.at(intcode, a) + Enum.at(intcode, b))
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, Enum.at(intcode, a) + Enum.at(intcode, b))
+      |> process_instruction(index + 4)
   end
   def process_instruction([101, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, a + Enum.at(intcode, b))
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, a + Enum.at(intcode, b))
+      |> process_instruction(index + 4)
   end
   def process_instruction([1001, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, Enum.at(intcode, a) + b)
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, Enum.at(intcode, a) + b)
+      |> process_instruction(index + 4)
   end
   def process_instruction([1101, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, a + b)
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, a + b)
+      |> process_instruction(index + 4)
   end
   def process_instruction([2, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, Enum.at(intcode, a) * Enum.at(intcode, b))
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, Enum.at(intcode, a) * Enum.at(intcode, b))
+      |> process_instruction(index + 4)
   end
   def process_instruction([102, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, a * Enum.at(intcode, b))
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, a * Enum.at(intcode, b))
+      |> process_instruction(index + 4)
   end
   def process_instruction([1002, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, Enum.at(intcode, a) * b)
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, Enum.at(intcode, a) * b)
+      |> process_instruction(index + 4)
   end
   def process_instruction([1102, a, b, c | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, c, a * b)
-    process_instruction(index + 4, new_intcode)
+    intcode
+      |> List.replace_at(c, a * b)
+      |> process_instruction(index + 4)
   end
   def process_instruction([3, a | _tail], index, intcode) do
-    new_intcode = List.replace_at(intcode, a, 1)
-    process_instruction(index + 2, new_intcode)
+    intcode
+      |> List.replace_at(a, system_id)
+      |> process_instruction(index + 2)
   end
   def process_instruction([4, a | _tail], index, intcode) do
     intcode
       |> Enum.at(a)
       |> IO.inspect
-    process_instruction(index + 2, intcode)
+    process_instruction(intcode, index + 2)
   end
   def process_instruction([104, a | _tail], index, intcode) do
-    process_instruction(index + 2, intcode)
+    IO.inspect(a)
+    process_instruction(intcode, index + 2)
+  end
+  def process_instruction([head | _tail], index, intcode) do
+    IO.inspect("Unknown opcode: " <> Integer.to_string(head))
+    intcode
   end
 
   def process_intcode(intcode) do

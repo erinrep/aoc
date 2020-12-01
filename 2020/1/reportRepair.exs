66df2db123906162790bb2a049d1683fc4fee2cb
Erin Replogle <erinrep@gmail.com>
2020-12-01 15:40:01 -0700

    AoC 2020 Dat 1 - part 2 - slow

--- 2020/1/reportRepair.exs
+++ 2020/1/reportRepair.exs
@@ -7,6 +7,7 @@ case File.read("expenses.txt") do
       |> String.split("\n", trim: true)
       |> Enum.map(&(String.to_integer(&1)))
 
+    IO.puts "Part 1"
     expenses
       |> Enum.flat_map(fn expense ->
         case Enum.find(expenses, &(expense + &1 == sum_to_find)) do
@@ -16,6 +17,28 @@ case File.read("expenses.txt") do
       end)
       |> List.first
       |> IO.inspect
+
+    IO.puts "Part 2"
+    indexes = expenses
+      |> Enum.map(fn expense ->
+        Enum.map(expenses, &(expense + &1))
+      end)
+      |> Enum.with_index
+      |> Enum.flat_map(fn {sums, index} ->
+        sums
+        |> Enum.with_index
+        |> Enum.flat_map(fn {sum, inner_index} -> 
+          expenses
+            |> Enum.map(&({sum + &1, index, inner_index}))
+            |> Enum.with_index
+        end)
+      end)
+      |> Enum.find(fn {{sum, _, _}, _} -> 
+        sum == sum_to_find
+      end)
+
+      {{sum_to_find, a, b}, c} = indexes
+      IO.inspect(Enum.at(expenses, a) * Enum.at(expenses, b) * Enum.at(expenses, c))
     
   {:error, :enoent} -> nil
 end

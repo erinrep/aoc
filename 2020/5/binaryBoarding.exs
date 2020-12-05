f42d45348ddff85a3fa86d794a2098b2a25eb124
Erin Replogle <erinrep@gmail.com>
2020-12-04 23:19:56 -0700

    AoC Day 5 - Part 2

--- 2020/5/binaryBoarding.exs
+++ 2020/5/binaryBoarding.exs
@@ -5,9 +5,10 @@ case File.read("seats.txt") do
     seats = contents
       |> String.split("\n", trim: true)
 
-    IO.puts "Part 1"
     total_rows = 127
     total_cols = 7
+
+    IO.puts "Part 1"
     seats
       |> Enum.map(fn seat ->
         {row, _, col, _} = seat
@@ -24,6 +25,30 @@ case File.read("seats.txt") do
       end)
       |> Enum.max
       |> IO.inspect
+
+    IO.puts "Part 2"
+    actual_seats = seats
+      |> Enum.map(fn seat ->
+        {row, _, col, _} = seat
+          |> String.split("", trim: true)
+          |> Enum.reduce({0, total_rows, 0, total_cols}, fn half, {min_row, max_row, min_col, max_col} ->
+            case half do
+              "B" -> {max_row - floor((max_row - min_row) / 2), max_row, min_col, max_col}
+              "F" -> {min_row, floor((max_row - min_row) / 2) + min_row, min_col, max_col}
+              "R" -> {min_row, max_row, max_col - floor((max_col - min_col) / 2), max_col}
+              "L" -> {min_row, max_row, min_col, floor((max_col - min_col) / 2) + min_col}
+            end
+          end)
+        row * 8 + col
+      end)
+      |> Enum.sort
+      
+      Enum.min(actual_seats)..Enum.max(actual_seats)
+        |> MapSet.new
+        |> MapSet.difference(MapSet.new(actual_seats))
+        |> MapSet.to_list
+        |> List.first
+        |> IO.inspect
     
   {:error, :enoent} -> nil
 end

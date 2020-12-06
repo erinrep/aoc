68c037506ad1c7e20159689dcbd3c878cad62109
Erin Replogle <erinrep@gmail.com>
2020-12-06 09:36:24 -0700

    AoC 2020 - Day 6 - Part 2

--- 2020/3/tobogganTrajectory.exs
+++ 2020/3/tobogganTrajectory.exs
@@ -22,5 +22,68 @@ case File.read("map.txt") do
       |> Enum.count(&(&1 == "#"))
       |> IO.inspect
 
+    # IO.puts "Part 2 1,1"
+    # rows
+    #   |> Enum.with_index
+    #   |> Enum.map(fn {row, index} ->
+
+    #     row
+    #       |> List.duplicate(80) #make dynamic
+    #       |> Enum.concat
+    #       |> Enum.at(index)
+
+    #   end)
+    #   |> Enum.count(&(&1 == "#"))
+    #   |> IO.inspect
+
+    # IO.puts "Part 2 5,1"
+    # rows
+    #   |> Enum.with_index
+    #   |> Enum.map(fn {row, index} ->
+    #     position = index * 5
+
+    #     row
+    #       |> List.duplicate(80) #make dynamic
+    #       |> Enum.concat
+    #       |> Enum.at(position)
+
+    #   end)
+    #   |> Enum.count(&(&1 == "#"))
+    #   |> IO.inspect
+
+    # IO.puts "Part 2 7,1"
+    # rows
+    #   |> Enum.with_index
+    #   |> Enum.map(fn {row, index} ->
+    #     position = index * 7
+
+    #     row
+    #       |> List.duplicate(80) #make dynamic
+    #       |> Enum.concat
+    #       |> Enum.at(position)
+
+    #   end)
+    #   |> Enum.count(&(&1 == "#"))
+    #   |> IO.inspect
+
+    # IO.puts "Part 2 1,2"
+    # rows
+    #   |> Enum.with_index
+    #   |> Enum.map(fn {row, index} ->
+    #     case Integer.mod(index, 2) do
+    #       1 -> "nope"
+    #       0 -> row
+    #             |> List.duplicate(80) #make dynamic
+    #             |> Enum.concat
+    #             |> Enum.at(Kernel.round(index / 2))
+    #     end
+
+    #   end)
+    #   |> Enum.count(&(&1 == "#"))
+    #   #|> Enum.count(&(&1 == nil))
+    #   |> IO.inspect
+
+    #   IO.inspect(276 * 100 * 85 * 90 * 37)
+
   {:error, :enoent} -> nil
 end
--- 2020/6/customCustoms.exs
+++ 2020/6/customCustoms.exs
@@ -1,14 +1,14 @@
-IO.puts "AoC 2020 - Day 3: Binary Boarding"
+IO.puts "AoC 2020 - Day 6: Custom Customs"
 
 case File.read("answers.txt") do
   {:ok, contents} ->
-    answers = contents
+    groups = contents
       |> String.split("\n\n", trim: true)
 
     IO.puts "Part 1"
-    answers
-      |> Enum.map(fn answer ->
-        answer
+    groups
+      |> Enum.map(fn group ->
+        group
           |> String.split("\n", trim: true)
           |> Enum.flat_map(&(String.split(&1, "", trim: true)))
           |> Enum.uniq
@@ -16,6 +16,25 @@ case File.read("answers.txt") do
       end)
       |> Enum.sum
       |> IO.inspect
-    
+
+    IO.puts "Part 2"
+    groups
+      |> Enum.map(fn group ->
+        answers = String.split(group, "\n", trim: true)
+        num_in_group = Enum.count(answers)
+        answers
+          |> Enum.flat_map(&(String.split(&1, "", trim: true)))
+          |> Enum.frequencies
+          |> Map.to_list
+          |> Enum.reduce(0, fn {_q, num}, acc ->
+            case num == num_in_group do
+              true -> acc + 1
+              false -> acc
+            end
+          end)
+      end)
+      |> Enum.sum
+      |> IO.inspect
+
   {:error, :enoent} -> nil
 end

b8ce67ede679985fab35d415758726a338761ae5
Erin Replogle <erinrep@gmail.com>
2020-12-07 15:38:21 -0700

    AoC 2020: Day 7 - Part 2

--- 2020/7/handyHaversacks.exs
+++ 2020/7/handyHaversacks.exs
@@ -15,6 +15,30 @@ defmodule Helpers do
       Helpers.find_parent_bags(rules, bag)
     end))
   end
+
+  def find_total_bags(_rules, 0), do: 0
+  def find_total_bags(rules, {num, bag}) do
+    bags = Enum.flat_map(rules, fn rule ->
+      [parent, child] = String.split(rule, ~r/contain[s]* /)
+      
+      case String.contains?(parent, bag) do 
+        true -> case child do
+                  "no other bags." -> []
+                  _ -> child
+                        |> String.split(", ")
+                        |> Enum.flat_map(fn c -> 
+                          [n, b] = Regex.split(~r{ }, String.replace(c, ~r/ bag.+/, ""), parts: 2)
+                          [{String.to_integer(n), b}]
+                        end)
+                end
+        false -> [] 
+      end
+    end)
+
+    num * Enum.reduce(bags, 1, fn bag_with_num, acc -> 
+      acc + Helpers.find_total_bags(rules, bag_with_num)
+    end)
+  end
 end
 
 case File.read("rules.txt") do
@@ -29,6 +53,12 @@ case File.read("rules.txt") do
       |> Enum.sort
       |> Enum.dedup
       |> Enum.count
+      |> IO.inspect
+
+    IO.puts "Part 2"
+    rules
+      |> Helpers.find_total_bags({1, my_bag})
+      |> Kernel.-(1)
       |> IO.inspect
 
   {:error, :enoent} -> nil

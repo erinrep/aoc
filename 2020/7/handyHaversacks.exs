e322d45278b913025639a92a2f8e4342461adefb
Erin Replogle <erinrep@gmail.com>
2020-12-07 14:23:21 -0700

    AoC 2020: Day 7 - Part 1

---
+++ 2020/7/handyHaversacks.exs
@@ -0,0 +1,36 @@
+IO.puts "AoC 2020 - Day 7: Handy Haversacks"
+
+defmodule Helpers do
+  def find_parent_bags(_rules, []), do: []
+  def find_parent_bags(rules, bag) do
+    bags = rules
+      |> Enum.flat_map(fn rule ->
+        [parent, child] = String.split(rule, "contain")
+        case String.contains?(child, bag) do 
+          true ->[String.replace(parent, ~r/ bag.+/, "")] 
+          false -> [] 
+        end
+      end)
+
+    Enum.concat(bags, Enum.flat_map(bags, fn a -> 
+      Helpers.find_parent_bags(rules, a)
+    end))
+  end
+end
+
+case File.read("rules.txt") do
+  {:ok, contents} ->
+    rules = contents
+      |> String.split("\n", trim: true)
+
+    my_bag = "shiny gold"
+
+    IO.puts "Part 1"
+    Helpers.find_parent_bags(rules, my_bag)
+      |> Enum.sort
+      |> Enum.dedup
+      |> Enum.count
+      |> IO.inspect
+
+  {:error, :enoent} -> nil
+end

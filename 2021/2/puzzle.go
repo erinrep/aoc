7a71e6c3e018debfe5442bce756a41e2ca66e1d1
Erin Replogle <erinrep@gmail.com>
2021-12-02 07:55:05 -0700

    AoC 2021: Day 2 - Part 2

--- 2021/2/puzzle.go
+++ 2021/2/puzzle.go
@@ -8,17 +8,11 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("Day 2: Dive!")
-
-	file, _ := os.Open("input.txt")
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+func partOne(directions [][]string) {
 	var depth int64 = 0
 	var distance int64 = 0
 
-	for scanner.Scan() {
-		direction := strings.Split(scanner.Text(), " ")
+	for _, direction := range directions {
 		num, _ := strconv.ParseInt(direction[1], 0, 64)
 		command := direction[0]
 		if command == "forward" {
@@ -30,5 +24,41 @@ func main() {
 		}
 	}
 
-	fmt.Println(depth * distance)
+	fmt.Println(fmt.Sprintf("Part One: %d", depth*distance))
+}
+
+func partTwo(directions [][]string) {
+	var aim int64 = 0
+	var depth int64 = 0
+	var distance int64 = 0
+
+	for _, direction := range directions {
+		num, _ := strconv.ParseInt(direction[1], 0, 64)
+		command := direction[0]
+		if command == "forward" {
+			distance += num
+			depth += (aim * num)
+		} else if command == "down" {
+			aim += num
+		} else {
+			aim -= num
+		}
+	}
+
+	fmt.Println(fmt.Sprintf("Part Two: %d", depth*distance))
+}
+
+func main() {
+	fmt.Println("Day 2: Dive!")
+
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var directions [][]string
+	for scanner.Scan() {
+		directions = append(directions, strings.Split(scanner.Text(), " "))
+	}
+
+	partOne(directions)
+	partTwo(directions)
 }

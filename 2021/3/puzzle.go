1730d35162395113a9d37ed2803fac747726c728
erinrep <erinrep@gmail.com>
2021-12-03 08:18:55 -0700

    AoC 2021: Day 3 - Part 2

--- 2021/3/puzzle.go
+++ 2021/3/puzzle.go
@@ -8,18 +8,7 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("Day 3: Binary Diagnostic")
-
-	file, _ := os.Open("input.txt")
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	var diagnostics [][]string
-
-	for scanner.Scan() {
-		diagnostics = append(diagnostics, strings.Split(scanner.Text(), ""))
-	}
-
+func partOne(diagnostics [][]string) {
 	var gammaRate []string
 	var epsilonRate []string
 	for i := 0; i < len(diagnostics[0]); i++ {
@@ -44,4 +33,72 @@ func main() {
 	epsilonRateDecimal, _ := strconv.ParseInt(strings.Join(epsilonRate, ""), 2, 64)
 
 	fmt.Println(fmt.Sprintf("Part One: %d", gammaRateDecimal*epsilonRateDecimal))
+}
+
+func numBitsInPosition(bits [][]string, position int) (int, int) {
+	ones := 0
+	zeros := 0
+	for _, b := range bits {
+		if b[position] == "1" {
+			ones++
+		} else {
+			zeros++
+		}
+	}
+	return ones, zeros
+}
+
+func matchingBitsInPosition(vals [][]string, position int, toMatch string) [][]string {
+	var newVals [][]string
+	for _, x := range vals {
+		if toMatch == x[position] {
+			newVals = append(newVals, x)
+		}
+	}
+	return newVals
+}
+
+func partTwo(diagnostics [][]string) {
+	var oxygenGeneratorRating [][]string = diagnostics
+	var co2ScrubberRating [][]string = diagnostics
+	var ones = 0
+	var zeros = 0
+	for i := 0; i < len(diagnostics[0]); i++ {
+		oxygenGeneratorRatingToMatch := "0"
+		ones, zeros = numBitsInPosition(oxygenGeneratorRating, i)
+		if ones >= zeros {
+			oxygenGeneratorRatingToMatch = "1"
+		}
+		if len(oxygenGeneratorRating) > 1 {
+			oxygenGeneratorRating = matchingBitsInPosition(oxygenGeneratorRating, i, oxygenGeneratorRatingToMatch)
+		}
+
+		co2ScrubberRatingToMatch := "0"
+		ones, zeros = numBitsInPosition(co2ScrubberRating, i)
+		if ones < zeros {
+			co2ScrubberRatingToMatch = "1"
+		}
+		if len(co2ScrubberRating) > 1 {
+			co2ScrubberRating = matchingBitsInPosition(co2ScrubberRating, i, co2ScrubberRatingToMatch)
+		}
+	}
+	ogr, _ := strconv.ParseInt(strings.Join(oxygenGeneratorRating[0], ""), 2, 64)
+	co2sr, _ := strconv.ParseInt(strings.Join(co2ScrubberRating[0], ""), 2, 64)
+	fmt.Println(fmt.Sprintf("Part Two: %d", ogr*co2sr))
+}
+
+func main() {
+	fmt.Println("Day 3: Binary Diagnostic")
+
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var diagnostics [][]string
+
+	for scanner.Scan() {
+		diagnostics = append(diagnostics, strings.Split(scanner.Text(), ""))
+	}
+
+	partOne(diagnostics)
+	partTwo(diagnostics)
 }

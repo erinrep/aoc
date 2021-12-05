1d9fa8cbfbeac302aaac8e5735611700084a04ce
Erin Replogle <erinrep@gmail.com>
2021-12-04 17:07:55 -0700

    AoC 2021: Day 4 - Part 2

--- 2021/4/puzzle.go
+++ 2021/4/puzzle.go
@@ -24,9 +24,7 @@ func contains(nums []int, val int) bool {
 	return false
 }
 
-func main() {
-	fmt.Println("Day 4: Giant Squid")
-
+func partOne() {
 	file, _ := os.Open("input.txt")
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -83,4 +81,79 @@ func main() {
 		}
 	}
 	fmt.Println(fmt.Sprintf("Part One: %d", score*winningNum))
+}
+
+func partTwo() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var cards []card
+	var winners []int
+	var cardNums [][]string
+	var numbers []string
+	i := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if i == 0 {
+			numbers = strings.Split(line, ",")
+		} else if line != "" {
+			cardNums = append(cardNums, strings.Fields(line))
+		} else if len(cardNums) > 0 {
+			cards = append(cards, card{numbers: cardNums})
+			winners = append(winners, 0)
+			cardNums = nil
+		}
+		i++
+	}
+	cards = append(cards, card{numbers: cardNums})
+	winners = append(winners, 0)
+
+	var winningCard [][]string
+	var winningNum int64
+	isFinalWinner := false
+	for _, n := range numbers {
+		for cardNum, c := range cards {
+			for y, row := range c.numbers {
+				for x, num := range row {
+					if n == num {
+						c.rowCounts[y] += 1
+						c.columnCounts[x] += 1
+						c.numbers[y][x] = "X"
+						cards[cardNum] = c
+					}
+				}
+			}
+			if contains(c.columnCounts[:], 5) || contains(c.rowCounts[:], 5) {
+				winningCard = c.numbers
+				winningNum, _ = strconv.ParseInt(n, 0, 64)
+				winners[cardNum] = 1
+				if !contains(winners, 0) {
+					isFinalWinner = true
+					break
+				}
+			}
+		}
+		if isFinalWinner {
+			break
+		}
+	}
+
+	var score int64 = 0
+	for _, row := range winningCard {
+		for _, s := range row {
+			if s != "X" {
+				n, _ := strconv.ParseInt(s, 0, 64)
+				score += n
+			}
+		}
+	}
+
+	fmt.Println(fmt.Sprintf("Part Two: %d", score*winningNum))
+}
+
+func main() {
+	fmt.Println("Day 4: Giant Squid")
+	partOne()
+	partTwo()
 }

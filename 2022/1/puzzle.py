33d7d532e1a6f3011444112673fb39e2c134f9e5
Erin Replogle <erinrep@gmail.com>
2022-11-30 22:24:47 -0700

    AoC 2022: Day 1

---
+++ 2022/1/puzzle.py
@@ -0,0 +1,22 @@
+print("Day 1: Calorie Counting")
+
+with open('input.txt', encoding="utf-8") as f:
+  snacks = list(f)
+
+calories = [0]
+i = 0
+for c in range(len(snacks)):
+  if snacks[c] == '\n':
+    i += 1
+    calories.append(0)
+  else:
+    calories[i] += int(snacks[c])
+
+print("Part 1: ", max(calories))
+
+NUM_ELVES = 3
+total = 0
+for i in range(NUM_ELVES):
+  total += int(calories.pop(calories.index(max(calories))))
+
+print("Part 2: ", total)

ba0b3b351733bb1dfdeb00a9848e1e08212ad517
Erin Replogle <erinrep@gmail.com>
2022-12-02 08:42:40 -0700

    use dicts

--- 2022/2/puzzle.py
+++ 2022/2/puzzle.py
@@ -11,65 +11,62 @@ self_paper = draw = 'Y'
 self_scissors = win = 'Z'
 draw_score = 3
 win_score = 6
-rock_score = 1
-paper_score = 2
-scissors_score = 3
+loss_score = 0
+
+outcomes = {
+  opponent_rock: {
+    self_rock: draw_score,
+    self_paper: win_score,
+    self_scissors: loss_score
+  },
+  opponent_paper: {
+    self_rock: loss_score,
+    self_paper: draw_score,
+    self_scissors: win_score
+  },
+  opponent_scissors: {
+    self_rock: win_score,
+    self_paper: loss_score,
+    self_scissors: draw_score
+  }
+}
+play_scores = {
+  self_rock: 1,
+  self_paper: 2,
+  self_scissors: 3
+}
 
 totalScore = 0
-
 for r in range(len(strategy)):
-  score = 0
   plays = strategy[r].replace('\n', '').split(" ")
-  if (plays[0] == opponent_rock and plays[1] == self_rock) or (plays[0] == opponent_paper and plays[1] == self_paper) or (plays[0] == opponent_scissors and plays[1] == self_scissors):
-    score += draw_score
-  else:
-    if (plays[0] == opponent_rock and plays[1] == self_paper) or (plays[0] == opponent_paper and plays[1] == self_scissors) or (plays[0] == opponent_scissors and plays[1] == self_rock):
-      score += win_score
-
-  if plays[1] == self_rock:
-    score += rock_score
-  elif plays[1] == self_paper:
-    score += paper_score
-  elif plays[1] == self_scissors:
-    score += scissors_score
-
-  totalScore += score
+  totalScore += outcomes[plays[0]][plays[1]]
+  totalScore += play_scores[plays[1]]
 
 print("Part One: ", totalScore)
 
+what_to_play = {
+  draw: {
+    opponent_rock: self_rock,
+    opponent_paper: self_paper,
+    opponent_scissors: self_scissors 
+  },
+  win: {
+    opponent_rock: self_paper,
+    opponent_paper: self_scissors,
+    opponent_scissors: self_rock
+  },
+  lose: {
+    opponent_rock: self_scissors,
+    opponent_paper: self_rock,
+    opponent_scissors: self_paper
+  }
+}
+
 totalScore = 0
 for r in range(len(strategy)):
-  score = 0
   plays = strategy[r].replace('\n', '').split(" ")
-  
-  if plays[0] == opponent_rock:
-    if plays[1] == lose:
-      score += scissors_score
-    elif plays[1] == draw:
-      score += draw_score
-      score += rock_score
-    elif plays[1] == win:
-      score += win_score
-      score += paper_score
-  elif plays[0] == opponent_paper:
-    if plays[1] == lose:
-      score += rock_score
-    elif plays[1] == draw:
-      score += draw_score
-      score += paper_score
-    elif plays[1] == win:
-      score += win_score
-      score += scissors_score
-  elif plays[0] == opponent_scissors:
-    if plays[1] == lose:
-      score += paper_score
-    elif plays[1] == draw:
-      score += draw_score
-      score += scissors_score
-    elif plays[1] == win:
-      score += win_score
-      score += rock_score
+  my_play = what_to_play[plays[1]][plays[0]]
+  totalScore += outcomes[plays[0]][my_play]
+  totalScore += play_scores[my_play]
 
-  totalScore += score
-
-print("Part 2: ", totalScore)
+print("Part Two: ", totalScore)

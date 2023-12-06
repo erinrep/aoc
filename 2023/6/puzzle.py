abe79d676bd1e746e214e08907ff4b7a3a6e2ff7
Erin Replogle <erinrep@gmail.com>
2023-12-06 08:01:05 -0700

    23 day 6 cleanup

--- 2023/6/puzzle.py
+++ 2023/6/puzzle.py
@@ -1,4 +1,5 @@
 import re
+from math import prod
 
 print("Day 6: Wait For It")
 
@@ -6,18 +7,14 @@ with open("input.txt", encoding="utf-8") as f:
     [times, distances] = [re.findall(r"[0-9]+", l) for l in list(f)]
 
 totals = []
-for i, t in enumerate(times):
+for i, raceTime in enumerate(times):
     winningHoldTimes = []
-    for h in range(1, int(t) - 1):
-        if (int(t) - h) * h > int(distances[i]):
-            winningHoldTimes.append(h)
+    for holdTime in range(1, int(raceTime) - 1):
+        if (int(raceTime) - holdTime) * holdTime > int(distances[i]):
+            winningHoldTimes.append(holdTime)
     totals.append(len(winningHoldTimes))
 
-total = 1
-for t in totals:
-    total *= t
-
-print("Part 1: ", total)
+print("Part 1: ", prod(totals))
 
 time = int("".join(times))
 distance = int("".join(distances))
